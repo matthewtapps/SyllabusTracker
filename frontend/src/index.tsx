656c4a559e2043c072edd213ccb487033709b6c6
matty-c0des <matt.tapps@gmail.com>
2023-10-06 15:57:25 +1100

    Frontend development

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import './css/reset.css';
+import './css/main.css';
+import './css/fonts.css'
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
---
+++ frontend/src/pages/LoginPage.tsx
@@ -0,0 +1,47 @@
+import React, { useState } from 'react';
+import Button from '@mui/material/Button'
+import '../css/main.css'
+import { Navigate } from 'react-router';
+
+const LoginPage: React.FC = () => {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    async function handleSubmit(event: React.FormEvent) {
+        event.preventDefault();
+        <Navigate to="/"></Navigate>
+        };
+
+    return (
+        <div className="login-container">
+            <form onSubmit={handleSubmit}>
+            <svg width="200" height="200" xmlns="http://www.w3.org/2000/svg" className="dominance-triangle">
+                <path d="M 110 10 L 200 190 L 20 190 Z"/>
+            </svg>
+                <div>
+                    <label htmlFor="username">Username: </label>
+                    <input
+                        type="text"
+                        id="username"
+                        value={username}
+                        onChange={(e) => setUsername(e.target.value)}
+                        autoComplete="username"
+                        />
+                </div>
+                <div>
+                    <label htmlFor="password">Password: </label>
+                    <input
+                        type="password"
+                        id="password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                        autoComplete="current-password"
+                        />
+                </div>
+                <Button variant="outlined" type="submit">Login</Button>
+            </form>
+        </div>
+    );
+};
+
+export default LoginPage;

62b9ddaeecec1ee75b9fc1d08b6961a7e14b4a26
Matthew Tapps <matt.tapps@gmail.com>
2023-10-08 17:25:11 +1100

    Sanitize dummy data

---
+++ shared/utils/fetchUser.ts
@@ -0,0 +1,15 @@
+import { User } from '../types/User'
+import { Role } from '../types/enums/Role'
+
+export function fetchUser(username: string, password: string): User {
+    let user: User = {
+        userId: 1,
+        role: Role.Student,
+        firstName: 'Example',
+        lastName: 'Student',
+        dateOfBirth: new Date(1963, 1, 24),
+        email: 'example@example.com',
+        mobile: '0400000000'
+    }
+    return user
+}

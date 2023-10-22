471f658678e18863c97b8cbb69d20bc8bd3b17fc
matty-c0des <matt.tapps@gmail.com>
2023-10-22 17:43:31 +1100

    Remove useless import from NavBar

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -1,6 +1,5 @@
 import { AppBar, Toolbar, IconButton, Typography } from "@mui/material"
 import MenuIcon from "@mui/icons-material/Menu"
-import theme from "../theme/Theme";
 
 const NavBar = (props: any) => {
     return (

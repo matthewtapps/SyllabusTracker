a39fd8ab9e6485ea3c82c272b6f50786a9abb774
matty-c0des <matt.tapps@gmail.com>
2024-01-06 12:53:57 +1100

    Merge rootReducer into store

--- frontend/src/store/store.ts
+++ frontend/src/store/store.ts
@@ -1,7 +1,24 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import { syllabusTrackerApi } from "../services/syllabusTrackerApi";
-import rootReducer from "./rootReducer";
+import authReducer from '../slices/auth';
+import collectionsReducer from '../slices/collections';
+import techniquesReducer from '../slices/techniques';
+import suggestionsReducer from '../slices/suggestions';
+import collectionTechniquesReducer from '../slices/collectionTechniques';
+import descriptionsReducer from '../slices/descriptions';
+import studentReducer from '../slices/student';
 
+
+const rootReducer = combineReducers({
+    auth: authReducer,
+    collections: collectionsReducer,
+    techniques: techniquesReducer,
+    suggestions: suggestionsReducer,
+    collectionTechniques: collectionTechniquesReducer,
+    descriptions: descriptionsReducer,
+    student: studentReducer,
+    [syllabusTrackerApi.reducerPath]: syllabusTrackerApi.reducer    
+});
 
 export const store = configureStore({
     reducer: rootReducer,
